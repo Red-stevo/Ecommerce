1f3e6c023f1cd9b3ac19aa224089c68ce52819da
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-17 13:58:03 +0300

    Commit message >>>>> working on general products display. <<<<<

--- onshop/src/IndexPage/Pages/ProductsDisplayPage/Components/StarRating.jsx
+++ onshop/src/IndexPage/Pages/ProductsDisplayPage/Components/StarRating.jsx
@@ -1,8 +1,8 @@
 import { FaStar } from "react-icons/fa";
 import { useState } from "react";
 
-const StarRating = ({active}) => {
-    const [rating, setRating] = useState(4);
+const StarRating = ({active, value}) => {
+    const [rating, setRating] = useState(value);
 
     return (
         <div style={{ display: "flex", cursor: "pointer" }}>
@@ -12,7 +12,7 @@ const StarRating = ({active}) => {
                     <label key={index} style={{ margin: "0 5px" }}>
                         <input type="radio" name="rate" id={`star-${currentRating}`} value={currentRating}
                             disabled={active}   style={{ display: "none" }} onClick={() => setRating(currentRating)} />
-                        <FaStar color={currentRating <= rating ? "#FFD700" : "grey"} size={20} />
+                        <FaStar color={currentRating <= rating ? "#FFD700" : "grey"} size={15} />
                     </label>
                 );
             })}
