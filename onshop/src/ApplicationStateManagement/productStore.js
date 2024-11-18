2eebd0452417f9239b5c3244331fe42c3fb6207f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-18 20:09:03 +0300

    Commit message >>>>> configuring product reduc config <<<<<

---
+++ onshop/src/ApplicationStateManagement/productStore.js
@@ -0,0 +1,24 @@
+import {createAsyncThunk, createEntityAdapter} from "@reduxjs/toolkit";
+
+const productStoreAdapter = createEntityAdapter();
+
+export const getProductDetails = createAsyncThunk("product/get-product",
+    async (productId, config) => {
+
+    });
+
+const initialState = productStoreAdapter.getInitialState({
+    productId:1,
+    productName:"J4 cactus jack sneakers",
+    productDescription:"The J4 Cactus Jack sneakers blend iconic Jordan 4 craftsmanship with Travis Scott's signature touch. Made from premium suede, they boast a striking colorway, bold Cactus Jack branding, and subtle details inspired by the rapper's vision. Combining superior comfort, durable construction, and a sleek design, these sneakers are perfect for sneakerheads, streetwear enthusiasts, or anyone seeking a bold, standout look. A true collector's item that effortlessly merges performance with head-turning style.",
+    productNewPrice:230,
+    productOldPrice:130,
+    productColor:"",
+    productSize:"",
+    productImage:[],
+    productCount:"",
+    products : [
+        {productId:1, productNewPrice:230, productOldPrice:130, productColor:"", productSize:"", productImages:[], productCount:""}
+    ]
+});
+
--- onshop/src/IndexPage/Pages/ProductDisplayPage/ProductDisplay.jsx
+++ onshop/src/IndexPage/Pages/ProductDisplayPage/ProductDisplay.jsx
@@ -1,19 +1,4 @@
 
-const productDetails = {
-    productId:1,
-    productName:"J4 cactus jack sneakers",
-    productDescription:"The J4 Cactus Jack sneakers blend iconic Jordan 4 craftsmanship with Travis Scott's signature touch. Made from premium suede, they boast a striking colorway, bold Cactus Jack branding, and subtle details inspired by the rapper's vision. Combining superior comfort, durable construction, and a sleek design, these sneakers are perfect for sneakerheads, streetwear enthusiasts, or anyone seeking a bold, standout look. A true collector's item that effortlessly merges performance with head-turning style.",
-    productNewPrice:230,
-    productOldPrice:130,
-    productColor:"",
-    productSize:"",
-    productImage:[],
-    productCount:"",
-    products : [
-{productId:1, productNewPrice:230, productOldPrice:130, productColor:"", productSize:"", productImages:[], productCount:""}
-    ]
-}
-
 const ProductDisplay = () => {
 
     return (
