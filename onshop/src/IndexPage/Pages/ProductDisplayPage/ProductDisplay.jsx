f9f8dde1f485677a9202277776497201304bfde9
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-19 22:06:12 +0300

    Commit message >>>>> intended functionality all done <<<<<

--- onshop/src/IndexPage/Pages/ProductDisplayPage/ProductDisplay.jsx
+++ onshop/src/IndexPage/Pages/ProductDisplayPage/ProductDisplay.jsx
@@ -1,103 +1,79 @@
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {getProductDetails} from "../../../ApplicationStateManagement/productStore.js";
-import {Button, Image, Pagination} from "react-bootstrap";
+import { useEffect, useState, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetails } from "../../../ApplicationStateManagement/productStore.js";
+import { Button, Image, Pagination } from "react-bootstrap";
 import "./ProductDisplay.css";
 
-
 const ProductDisplay = () => {
-    const {products, productName, productDescription} = useSelector(state => state.productReducer.product);
+    const { products, productName, productDescription } = useSelector(
+        (state) => state.productReducer.product
+    );
     const dispatch = useDispatch();
-    const [size, setSizes] = useState(new Set());
+
+    // State
     const [activeSize, setActiveSize] = useState(null);
-    const [availableProducts, setAvailableProducts] = useState([]);
-    const [availableColors, setAvailableColors] = useState([]);
     const [activeColor, setActiveColor] = useState(null);
     const [productOnDisplay, setProductOnDisplay] = useState(null);
 
 
-    /*Fetch product from the backend.*/
+    // Fetch products on component mount
     useEffect(() => {
         dispatch(getProductDetails(null));
+    }, [dispatch]);
 
-        /*Structure the product details.*/
-        const structureData = () => {
-            let sizes = [];
-            setSizes(new Set());
-            products.forEach(({ productSize }) => {
-                sizes.push(productSize);
-            });
-            setSizes(new Set(sizes.sort()));
-        }
-        if(products && products.length > 0 ) structureData();
-
-    }, [products, ]);
-
-    useEffect(() => {
-
-        /*Initializing the product display*/
-        const setInitialProduct = () => {
-
-            const {productColor, productSize} = products[0];
-            updateAvailableProducts(productSize);
-            updateProductOnDisplay(productColor);
-        }
-
-
-        if (size && Array.from(size).length > 0) {
-            setInitialProduct();
-        }
-    }, [size]);
+    // Extract unique sizes
+    const sizes = useMemo(() => {
+        if (!products || products.length === 0) return [];
+        return [...new Set(products.map((product) => product.productSize))].sort();
+    }, [products]);
 
 
 
-    const updateAvailableProducts = (activeSize) => {
-
-        setActiveSize(activeSize); /*Set the active product to update the hover event listener*/
-        setAvailableProducts([]); /*Reset the list of available products for the proportion*/
-
-
-        /*Get a list of products for a given proportion*/
-        if (size && Array.from(size).length > 0) {
-            products.forEach((product) => {
-                if (product.productSize === activeSize) setAvailableProducts(prevState => [...prevState, product]);
-            });
-        }
-    }
+    // Filter products by active size
+    const availableProducts = useMemo(() => {
+        if (!activeSize || !products) return [];
+        return products.filter((product) => product.productSize === activeSize);
+    }, [products, activeSize]);
 
 
-    useEffect(() => {
-        setAvailableColors([]);  /*Reset available colors*/
 
-        /*Get the list available varieties.*/
-        if (availableProducts && availableProducts.length > 0) {
-            availableProducts.forEach((product) => {
-                setAvailableColors(prevState => [...prevState, product.productColor]);
-            });
-        }
+    // Extract colors from available products
+    const availableColors = useMemo(() => {
+        return availableProducts.map((product) => product.productColor);
     }, [availableProducts]);
 
 
-    const updateProductOnDisplay = (color) => {
 
-        /*Update the color toggle*/
-        setActiveColor(color);
-
-        /*Set Product to be display to the user.*/
-        availableProducts.forEach((product) => {
-            if (product.productColor === color) setProductOnDisplay(product);
-        });
-    }
-
-
-
-        /*Testing*/
+    // Initialize active size, color, and product display
     useEffect(() => {
-        if(productOnDisplay) {
-            console.log(productOnDisplay);
-            console.log(productOnDisplay.productImages);
+        if (products && products.length > 0) {
+            const { productSize, productColor } = products[0];
+            setActiveSize(productSize);
+            setActiveColor(productColor);
         }
-    }, [productOnDisplay]);
+    }, [products]);
+
+
+
+    // Update displayed product when active color changes
+    useEffect(() => {
+        if (activeColor) {
+            const product = availableProducts.find(
+                (product) => product.productColor === activeColor
+            );
+            setProductOnDisplay(product || null);
+        }
+    }, [activeColor, availableProducts]);
+
+
+
+    // Automatically set the first available color if the active size changes
+    useEffect(() => {
+        if (availableColors.length > 0) {
+            setActiveColor(availableColors[0]);
+        }
+    }, [availableColors]);
+
 
 
     return (
@@ -106,9 +82,16 @@ const ProductDisplay = () => {
                 <div>
                     <Image height={160} width={140} />
                     <div>
-                        {productOnDisplay && productOnDisplay.productImages && productOnDisplay.productImages.map((imageUrl, index) => (
-                            <Image src={imageUrl} height={30} width={20} key={index} alt={`product image ${index}`}  />
-                        ))}
+                        {productOnDisplay &&
+                            productOnDisplay.productImages.map((imageUrl, index) => (
+                                <Image
+                                    src={imageUrl}
+                                    height={30}
+                                    width={20}
+                                    key={index}
+                                    alt={`product image ${index}`}
+                                />
+                            ))}
                     </div>
                 </div>
                 <div>
@@ -116,51 +99,46 @@ const ProductDisplay = () => {
                         <Button>Details</Button>
                         <Button>Reviews</Button>
                     </div>
-                    <span>
-                        {productName}
-                    </span>
-                    <section>
-                        {productDescription}
-                    </section>
+                    <span>{productName}</span>
+                    <section>{productDescription}</section>
                     <div>
                         <div>
                             <span>
-                                Proportion :
+                                Proportion:
                                 <Pagination>
-                                    {
-                                        Array.from(size).map((size, index) => (
-                                            <Pagination.Item key={index} active={activeSize === size}
-                                                             onClick={() => updateAvailableProducts(size)}>
-                                                {size}
-                                            </Pagination.Item>
-                                        ))
-                                    }
-                              </Pagination>
+                                    {sizes.map((size, index) => (
+                                        <Pagination.Item
+                                            key={index}
+                                            active={activeSize === size}
+                                            onClick={() => setActiveSize(size)}
+                                        >
+                                            {size}
+                                        </Pagination.Item>
+                                    ))}
+                                </Pagination>
                             </span>
                             <span>
-                               Variety :
-                                 <Pagination>
-                                    {
-                                        availableColors.map((color, index) => (
-                                            <Pagination.Item key={index} active={activeColor === color}
-                                                             onClick={() => updateProductOnDisplay(color)}>
-                                                {color}
-                                            </Pagination.Item>
-                                        ))
-                                    }
-                              </Pagination>
+                                Variety:
+                                <Pagination>
+                                    {availableColors.map((color, index) => (
+                                        <Pagination.Item
+                                            key={index}
+                                            active={activeColor === color}
+                                            onClick={() => setActiveColor(color)}
+                                        >
+                                            {color}
+                                        </Pagination.Item>
+                                    ))}
+                                </Pagination>
                             </span>
                         </div>
-                        <span>
-                            Price : {}
-                        </span>
+                        <span>Price:{productOnDisplay && productOnDisplay.productNewPrice}</span>
                     </div>
                 </div>
             </div>
             <div>
                 <span>Related Products</span>
             </div>
-
             <div>
                 <span>Product Reviews</span>
             </div>
