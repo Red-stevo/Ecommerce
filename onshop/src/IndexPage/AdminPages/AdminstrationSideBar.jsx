309672f9b43cf48f750ea7ead116162bf1b5a8c0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-23 15:26:50 +0300

    Commit message >>>>> responsive dashboard done <<<<<

--- onshop/src/IndexPage/AdminPages/AdminstrationSideBar.jsx
+++ onshop/src/IndexPage/AdminPages/AdminstrationSideBar.jsx
@@ -4,16 +4,20 @@ import {IoCloseCircleOutline} from "react-icons/io5";
 import {AiFillProduct} from "react-icons/ai";
 import MenuToggleComponent from "./Components/MenuToggleComponent.jsx";
 import {FaProductHunt} from "react-icons/fa";
+import {useState} from "react";
+import {PiListDashesFill} from "react-icons/pi";
 
 
 const AdministrationSideBar = () => {
+    const [hide, setHide] = useState(true);
 
     return (
         <>
-            <div className={"dash-board"}>
+            {hide && <PiListDashesFill className={"dash-board-toggle"} onClick={() => setHide(!hide)} /> }
+            <div className={`dash-board ${hide && "hide-element"}`}>
                 <span className={"admin-title"}>
                     ADMIN
-                    <IoCloseCircleOutline className={"close-button"} title={"Close"} />
+                    <IoCloseCircleOutline className={"close-button"} title={"Close"} onClick={() => setHide(!hide)} />
                 </span>
                 <span className={"dashboard-title"}>
                     <AiFillProduct className={"squares"} />
@@ -37,8 +41,7 @@ const AdministrationSideBar = () => {
             </div>
 
 
-            <div className={"admin-page-base"}>
-                <h1>Hello</h1>
+            <div className={"admin-page-base"} onClick={() => setHide(true)}>
                 <Outlet />
             </div>
         </>
