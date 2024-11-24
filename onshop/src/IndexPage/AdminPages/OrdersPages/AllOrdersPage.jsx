c6a91c526a8ac29d054c585a68d3d5fe34627717
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-24 20:09:24 +0300

    Commit message >>>>> all order page all done <<<<<

--- onshop/src/IndexPage/AdminPages/OrdersPages/AllOrdersPage.jsx
+++ onshop/src/IndexPage/AdminPages/OrdersPages/AllOrdersPage.jsx
@@ -1,7 +1,50 @@
 import "./Styles/AllOrdersPage.css";
+import {useEffect, useState} from "react";
+import {CiFilter} from "react-icons/ci";
+import {Button} from "react-bootstrap";
 
+const orders = [
+    {orderId:"FDR567NBHY", orderDate:"Sep Fri 13 2024", orderStatus:"CANCELED", orderTotal:2500.00},
+    {orderId:"FDR56EWBZY", orderDate:"Feb Mon 1 2024", orderStatus:"UNDELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567N5RY", orderDate:"Feb Mon 1 2024", orderStatus:"UNDELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567NBQY", orderDate:"Dec Fri 20 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR56EWBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567NBZY", orderDate:"Feb Mon 1 2024", orderStatus:"UNDELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567NBFY", orderDate:"Jan Fri 30 2024", orderStatus:"CANCELED", orderTotal:2500.00},
+    {orderId:"FDRAA7NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"CANCELED", orderTotal:2500.00},
+    {orderId:"FDR5KLNBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDMN67NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"SHIPPING", orderTotal:2500.00},
+    {orderId:"FDR517NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR501NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"SHIPPING", orderTotal:2500.00},
+    {orderId:"FDR56CXBBY", orderDate:"Nov Fri 19 2024", orderStatus:"SHIPPING", orderTotal:2500.00},
+    {orderId:"FDR5OPNBBY", orderDate:"Nov Fri 19 2024", orderStatus:"SHIPPING", orderTotal:2500.00},
+    {orderId:"FDOT67NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR567NODM", orderDate:"Nov Fri 19 2024", orderStatus:"SHIPPING", orderTotal:2500.00},
+    {orderId:"FPNU67NBBY", orderDate:"Nov Fri 19 2024", orderStatus:"DELIVERED", orderTotal:2500.00},
+    {orderId:"FDR5UDABBY", orderDate:"Nov Fri 19 2024", orderStatus:"UNDELIVERED", orderTotal:2500.00},
+]
+
+const statusList = ["UNDELIVERED", "SHIPPING","DELIVERED","CANCELED", "ALL"];
 
 const AllOrdersPage = () => {
+    const [orderDisplay, setOrderDisplay] = useState([]);
+    const [statusPointer, setStatusPointer] = useState(0);
+
+    useEffect(() => {
+
+        if (statusPointer <= 3)
+            setOrderDisplay(() => orders.filter((order) => (order.orderStatus === statusList[statusPointer])));
+        else
+            setOrderDisplay([...orders]);
+    }, [statusPointer, orders]);
+
+
+    const  handleFilter = ()  => {
+        if (statusPointer >= 4) setStatusPointer(0);
+        else setStatusPointer(prevState => prevState + 1);
+    }
+
     return (
         <div className={"all-orders-page"}>
             <div className={"all-orders-holder"}>
@@ -11,13 +54,23 @@ const AllOrdersPage = () => {
                     <span className={"order-status"}>Status</span>
                     <span className={"total-amount"}>Total</span>
                 </div>
-                <div className={"order-contents"}>
-                    <span className={"text"}>FDR567NBHY</span>
-                    <span className={"text"}>Nov Fri 20 2024</span>
-                    <span className={"text"}>Delivered</span>
-                    <span className={"cost-text text"}>ksh 2300.00</span>
+
+                <div className={"filter-component"}>
+                    <span>{statusList[statusPointer]}</span>
+                    <CiFilter className={"filter-icon"} onClick={handleFilter}/>
                 </div>
+
+                {orderDisplay.length > 0 && orderDisplay.map(({orderId, orderDate, orderStatus, orderTotal}) => (
+                    <div className={"order-contents"} key={orderId}>
+                        <span className={"text"}>{orderId}</span>
+                        <span className={"text"}>{orderDate}</span>
+                        <span className={"text"}>{orderStatus}</span>
+                        <span className={"cost-text text"}>ksh {orderTotal}</span>
+                    </div>
+                ))}
             </div>
+
+            <div className={"load-more-orders"}><Button className={"app-button"}>Load More</Button></div>
         </div>
     );
 };
