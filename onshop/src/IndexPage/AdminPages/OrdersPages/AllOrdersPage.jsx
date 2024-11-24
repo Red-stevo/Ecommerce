64997299046a34758a0ec8e1002e387071d1daf4
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-11-24 18:26:10 +0300

    Commit message >>>>> desktop done <<<<<

--- onshop/src/IndexPage/AdminPages/OrdersPages/AllOrdersPage.jsx
+++ onshop/src/IndexPage/AdminPages/OrdersPages/AllOrdersPage.jsx
@@ -3,8 +3,21 @@ import "./Styles/AllOrdersPage.css";
 
 const AllOrdersPage = () => {
     return (
-        <div>
-            <h1>All Order</h1>
+        <div className={"all-orders-page"}>
+            <div className={"all-orders-holder"}>
+                <div className={"headings"}>
+                    <span className={"order-num"}>Order ID</span>
+                    <span className={"order-date"}>Date</span>
+                    <span className={"order-status"}>Status</span>
+                    <span className={"total-amount"}>Total</span>
+                </div>
+                <div className={"order-contents"}>
+                    <span>FDR567NBHY</span>
+                    <span>Nov Fri 20 2024</span>
+                    <span>Delivered</span>
+                    <span>ksh 2300.00</span>
+                </div>
+            </div>
         </div>
     );
 };
