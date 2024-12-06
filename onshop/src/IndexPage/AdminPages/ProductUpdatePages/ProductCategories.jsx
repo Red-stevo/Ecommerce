5ddb43f34581694c10c2732953bfed9c2d1ab7dc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-06 06:27:42 +0300

    Commit message >>>>> worked on the page header <<<<<

--- onshop/src/IndexPage/AdminPages/ProductUpdatePages/ProductCategories.jsx
+++ onshop/src/IndexPage/AdminPages/ProductUpdatePages/ProductCategories.jsx
@@ -1,8 +1,17 @@
+import "./Styles/ProductCategories.css";
 
 const ProductCategories = () => {
     return (
         <div className={"product-categories-page"}>
-            <span className={"product-categories-header"}></span>
+            <span className={"product-categories-header"}>Product Categories</span>
+
+            <section className={"product-categories-body"}>
+                <div className={"product-categories-body-header"}>
+                    <span className={"product-categories-body-header-icon"}>Icon</span>
+                    <span className={"product-categories-body-header-category"}>Category</span>
+                </div>
+
+            </section>
         </div>
     );
 };
