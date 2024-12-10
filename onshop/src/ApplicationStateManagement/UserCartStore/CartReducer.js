30f219c85f0af10b22055e7b635147f126097e22
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-10 21:31:00 +0300

    Commit message >>>>> all done for the day <<<<<

--- onshop/src/ApplicationStateManagement/UserCartStore/CartReducer.js
+++ onshop/src/ApplicationStateManagement/UserCartStore/CartReducer.js
@@ -3,7 +3,14 @@ import {RequestsConfig} from "../RequestsConfig.js";
 
 const cartAdapter = createEntityAdapter();
 
-const initialState = cartAdapter.getInitialState({errorMessage:"", loading:"", success:"",});
+const initialState = cartAdapter.getInitialState(
+    {
+        errorMessage:"",
+        loading:"",
+        success:"",
+        CartResponse:{username:"", cartId:"", cartItemsResponses:[], youMayLikes:[], currentPage:0, totalPages:0,
+            hasMore:true}
+    });
 
 export const addToCart = createAsyncThunk("cart/addToCart",
     async (productData = null,
@@ -17,6 +24,18 @@ export const addToCart = createAsyncThunk("cart/addToCart",
         return rejectWithValue(error.response ? error.response.data : error.data);
     }
 });
+
+export const getCartItems = createAsyncThunk("cart/getCart",
+    async (productData = null, {fulfillWithValue,rejectWithValue}) => {
+
+        const {page, size, userId} = productData;
+
+        try {
+            fulfillWithValue((await RequestsConfig.post(`/customer/cart/${userId}?page=${page}&size=${size}`)).data);
+        }catch (error){
+            return rejectWithValue(error.response ? error.response.data : error.data);
+        }
+    });
 
 
 
@@ -37,6 +56,28 @@ const CartReducer = createSlice({
                 state.success = action.payload;
             })
             .addCase(addToCart.rejected, (state, action) => {
+                state.errorMessage = action.payload;
+                state.loading = false;
+                state.success = null;
+            })
+            .addCase(getCartItems.pending, (state) => {
+                state.success = null;
+                state.errorMessage = null;
+                state.loading = true;
+            })
+            .addCase(getCartItems.fulfilled, (state, action) => {
+                state.loading = false;
+                state.errorMessage = null;
+                state.success = true;
+                state.CartResponse.username=action.payload.username;
+                state.CartResponse.cartId=action.payload.cartId;
+                state.CartResponse.cartItemsResponses=action.payload.cartItemsResponses;
+                state.CartResponse.youMayLikes=[...state.CartResponse.youMayLikes,...action.payload.youMayLikes];
+                state.CartResponse.currentPage=action.payload.currentPage;
+                state.CartResponse.totalPages=action.payload.totalPages;
+                state.CartResponse.hasMore=action.payload.hasMore;
+            })
+            .addCase(getCartItems.rejected, (state, action) => {
                 state.errorMessage = action.payload;
                 state.loading = false;
                 state.success = null;
